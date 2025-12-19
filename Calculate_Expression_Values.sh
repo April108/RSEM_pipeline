f671e1d8f1273d674e24a5e48140ec415675c202
ZXY <april_20020108@outlook.com>
2025-12-19 19:45:04 +0800

    Change input files to uncompressed fastq format
    Updated input file names to uncompressed versions.

--- Calculate_Expression_Values.sh
+++ Calculate_Expression_Values.sh
@@ -9,7 +9,7 @@ rsem-calculate-expression --paired-end \
     --calc-ci \ # calculate 95% credibility intervals and posterior mean estimates
     --ci-memory 4096 \
     -p 30 \
-    ./SRR4421506_1.fastq.gz \
-    ./SRR4421506_2.fastq.gz \
+    ./SRR4421506_1.fastq \ # unzip gz files first
+    ./SRR4421506_2.fastq \ # Unknown option: --gzipped-read-file (however included in the --help examples)
     ./ref/human_gencode \ # path/prefix to the index
     SRR4421506_results # output prefix
