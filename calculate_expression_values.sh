d684afdea202e5750cc840cf39d00b517cf1ecf2
ZXY <april_20020108@outlook.com>
2025-12-19 21:15:48 +0800

    Update calculate_expression_values.sh

--- calculate_expression_values.sh
+++ calculate_expression_values.sh
@@ -4,7 +4,7 @@ rsem-calculate-expression --paired-end \
     --phred33-quals \
     --estimate-rspd \
     --seed 42 \ # set seed 
-    --output-genome-bam \
+    --output-genome-bam \ # fail if two mates are aligned to two different transcripts
     --sort-bam-by-coordinate \
     --calc-ci \ # calculate 95% credibility intervals and posterior mean estimates
     --ci-memory 4096 \
