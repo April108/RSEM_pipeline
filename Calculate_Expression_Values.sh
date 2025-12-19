83fcc7a1d66669dc6c6adbdee0deaa8487a951e1
ZXY <april_20020108@outlook.com>
2025-12-19 19:20:48 +0800

    Fix formatting and remove unnecessary line breaks

--- Calculate_Expression_Values.sh
+++ Calculate_Expression_Values.sh
@@ -1,16 +1,16 @@
 rsem-calculate-expression --paired-end \
-  --star \
-  --star-path /path/to/STAR \
-  --phred33-quals \
-  --gzipped-read-file \
-  --estimate-rspd \
-  --seed 42 \ # set seed 
-  --output-genome-bam \
-  --sort-bam-by-coordinate \
-  --calc-ci \ # calculate 95% credibility intervals and posterior mean estimates
-  --ci-memory 4096 \
-  -p 30 \
-  ./SRR4421506_1.fastq.gz \
-  ./SRR4421506_2.fastq.gz \
-  ./ref/human_gencode \ # path/prefix to the index
-  SRR4421506_results # output prefix
+    --star \
+    --star-path /path/to/STAR \
+    --phred33-quals \
+    --gzipped-read-file \
+    --estimate-rspd \
+    --seed 42 \ # set seed 
+    --output-genome-bam \
+    --sort-bam-by-coordinate \
+    --calc-ci \ # calculate 95% credibility intervals and posterior mean estimates
+    --ci-memory 4096 \
+    -p 30 \
+    ./SRR4421506_1.fastq.gz \
+    ./SRR4421506_2.fastq.gz \
+    ./ref/human_gencode \ # path/prefix to the index
+    SRR4421506_results # output prefix
